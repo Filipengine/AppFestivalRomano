f64c382ac09b74e5af061670a2b212f792f63654
Filipe Martins <filipe.martins@filipengine.pt>
2018-10-11 22:14:14 +0100

    no message

---
+++ Talabrigae/Talabrigae/Pages/Ligacao.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Talabrigae.Pages
+{
+    public partial class Ligacao : ContentPage
+    {
+        public Ligacao()
+        {
+            InitializeComponent();
+        }
+    }
+}
